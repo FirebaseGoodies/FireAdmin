3b9d88155a24df0a5c03d6c0c29a8f3553e2d394
AXeL-dev <contact.axel.dev@gmail.com>
2020-02-09 19:16:42 +0100

    Add firebase config

--- projects/demo/src/environments/environment.prod.ts
+++ projects/demo/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDM1ybQFpUe3fJ046KAfJeoiBHl5M51Rt0",
+    authDomain: "fireadmin-74e50.firebaseapp.com",
+    databaseURL: "https://fireadmin-74e50.firebaseio.com",
+    projectId: "fireadmin-74e50",
+    storageBucket: "fireadmin-74e50.appspot.com",
+    messagingSenderId: "1081829121945",
+    appId: "1:1081829121945:web:349839b2c6846c71143d08"
+  }
 };
--- projects/demo/src/environments/environment.ts
+++ projects/demo/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDM1ybQFpUe3fJ046KAfJeoiBHl5M51Rt0",
+    authDomain: "fireadmin-74e50.firebaseapp.com",
+    databaseURL: "https://fireadmin-74e50.firebaseio.com",
+    projectId: "fireadmin-74e50",
+    storageBucket: "fireadmin-74e50.appspot.com",
+    messagingSenderId: "1081829121945",
+    appId: "1:1081829121945:web:349839b2c6846c71143d08"
+  }
 };
 
 /*
