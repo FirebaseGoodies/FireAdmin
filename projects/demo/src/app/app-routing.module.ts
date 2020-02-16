82a13ec5b325c1c13b9ae803cb82744b1e18d308
AXeL-dev <contact.axel.dev@gmail.com>
2020-02-16 16:03:15 +0100

    Auto re-build library on demo app serve

--- projects/demo/src/app/app-routing.module.ts
+++ projects/demo/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: 'admin',
-    loadChildren: () => import('fire-admin').then(m => m.FireAdminModule)
+    //loadChildren: () => import('fire-admin').then(m => m.FireAdminModule)
+    loadChildren: () => import('projects/fire-admin/src/public-api').then(m => m.FireAdminModule)
   },
   {
     path: '**',
--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FireAdminModule } from 'fire-admin';
+//import { FireAdminModule } from 'fire-admin';
+import { FireAdminModule } from 'projects/fire-admin/src/public-api';
 
 @NgModule({
   declarations: [
