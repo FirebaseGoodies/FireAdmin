a467e9bdcceb2686ac03bc75e8d6f57d2e704d10
AXeL-dev <contact.axel.dev@gmail.com>
2020-02-21 21:43:56 +0100

    Set admin translations in separate files

---
+++ projects/fire-admin/src/lib/i18n/en.ts
@@ -0,0 +1,38 @@
+
+export const en = {
+  // Login page
+  "SignInToYourAccount": "Sign In to Your Account",
+  "EmailAddress": "Email address",
+  "EnterEmail": "Enter email",
+  "Password": "Password",
+  "RememberMe": "Remember me",
+  "SignIn": "Sign In",
+  "ForgotYourPassword": "Forgot your password?",
+  // Sidebar
+  "Dashboard": "Dashboard",
+  "Pages": "Pages",
+  "List": "List",
+  "Add": "Add",
+  "Posts": "Posts",
+  "Categories": "Categories",
+  "Translations": "Translations",
+  "Comments": "Comments",
+  "Menus": "Menus",
+  "Media": "Media",
+  // Navbar
+  "AddPost": "Add post",
+  "Logout": "Logout",
+  // Posts: Add
+  "AddNewPost": "Add New Post",
+  "PostTitle": "Post Title",
+  "PostContent": "Post Content",
+  "Actions": "Actions",
+  "PostStatus": "Status",
+  "PostDate": "Date",
+  "PostSlug": "Slug",
+  "Draft": "Draft",
+  "Published": "Published",
+  "SaveDraft": "Save Draft",
+  "Publish": "Publish",
+  "NewCategory": "New category",
+};
--- projects/fire-admin/src/lib/services/i18n.service.ts
+++ projects/fire-admin/src/lib/services/i18n.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { en } from '../i18n/en';
 
 @Injectable()
 export class I18nService {
@@ -7,43 +8,7 @@ export class I18nService {
   private translations: any = [];
 
   constructor() {
-    this.translations['en'] = {
-      // Login page
-      "SignInToYourAccount": "Sign In to Your Account",
-      "EmailAddress": "Email address",
-      "EnterEmail": "Enter email",
-      "Password": "Password",
-      "RememberMe": "Remember me",
-      "SignIn": "Sign In",
-      "ForgotYourPassword": "Forgot your password?",
-      // Sidebar
-      "Dashboard": "Dashboard",
-      "Pages": "Pages",
-      "List": "List",
-      "Add": "Add",
-      "Posts": "Posts",
-      "Categories": "Categories",
-      "Translations": "Translations",
-      "Comments": "Comments",
-      "Menus": "Menus",
-      "Media": "Media",
-      // Navbar
-      "AddPost": "Add post",
-      "Logout": "Logout",
-      // Posts: Add
-      "AddNewPost": "Add New Post",
-      "PostTitle": "Post Title",
-      "PostContent": "Post Content",
-      "Actions": "Actions",
-      "PostStatus": "Status",
-      "PostDate": "Date",
-      "PostSlug": "Slug",
-      "Draft": "Draft",
-      "Published": "Published",
-      "SaveDraft": "Save Draft",
-      "Publish": "Publish",
-      "NewCategory": "New category",
-    };
+    this.translations['en'] = en;
   }
 
   setLanguage(lang: string): void {
