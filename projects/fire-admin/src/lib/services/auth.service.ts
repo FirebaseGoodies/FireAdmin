88b232e8e0fdf3910e25e20de413739878b4f17a
AXeL-dev <contact.axel.dev@gmail.com>
2020-02-22 19:52:11 +0100

    Fix console warning
    It looks like you're using the development build of the Firebase JS SDK.

--- projects/fire-admin/src/lib/services/auth.service.ts
+++ projects/fire-admin/src/lib/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { first } from 'rxjs/operators';
-import { auth } from 'firebase';
+import { auth } from 'firebase/app';
 
 @Injectable()
 export class AuthService {
