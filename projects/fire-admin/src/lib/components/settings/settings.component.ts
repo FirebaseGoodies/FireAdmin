e320d0b31746e14fb0922b4430ad9b1a0e5d932f
AXeL-dev <contact.axel.dev@gmail.com>
2020-03-05 20:31:31 +0100

    Disallow using `id` key as a supported language key
    Since it will enter in conflict with document id

--- projects/fire-admin/src/lib/components/settings/settings.component.ts
+++ projects/fire-admin/src/lib/components/settings/settings.component.ts
@@ -3,6 +3,7 @@ import { I18nService } from '../../services/i18n.service';
 import { AlertService } from '../../services/alert.service';
 import { SettingsService } from '../../services/settings.service';
 import { NavigationService } from '../../services/navigation.service';
+import { Language } from '../../models/language.model';
 
 @Component({
   selector: 'fa-settings',
@@ -10,6 +11,9 @@ import { NavigationService } from '../../services/navigation.service';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+
+  i18nLanguage: string;
+  i18nKey: string;
 
   constructor(public settings: SettingsService, public navigation: NavigationService, private i18n: I18nService, private alert: AlertService) { }
 
@@ -21,6 +25,29 @@ export class SettingsComponent implements OnInit {
     this.settings.save();
     this.alert.success(this.i18n.get('SettingsSaved'), true);
     window.location.reload();
+  }
+
+  onI18nLanguageInput() {
+    if (!this.i18nKey || this.i18nKey.length < 2) {
+      this.i18nKey = this.i18nLanguage.substr(0, 2).toLowerCase();
+    }
+  }
+
+  addSupportedLanguage() {
+    this.settings.supportedLanguages.push({
+      label: this.i18nLanguage,
+      key: this.i18nKey,
+      isActive: true,
+      isRemovable: true
+    });
+    this.i18nLanguage = this.i18nKey = '';
+  }
+
+  removeSupportedLanguage(lang: Language, index: number) {
+    const activeSupportedLanguages = this.settings.getActiveSupportedLanguages();
+    if ((activeSupportedLanguages && activeSupportedLanguages.length > 1) || !lang.isActive) {
+      this.settings.supportedLanguages.splice(index, 1);
+    }
   }
 
 }
