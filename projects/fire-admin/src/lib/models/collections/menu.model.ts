e3d6072b4e151ca34ba3318d1ecafd23b75b1daa
AXeL-dev <contact.axel.dev@gmail.com>
2020-03-15 00:28:10 +0100

    Add menu model

---
+++ projects/fire-admin/src/lib/models/collections/menu.model.ts
@@ -0,0 +1,14 @@
+
+export interface Menu {
+  id?: string,
+  name: string,
+  lang: string,
+  items: MenuItem[]
+}
+
+export interface MenuItem {
+  title: string,
+  url: string,
+  //icon?: string,
+  childrens: MenuItem[]
+}
